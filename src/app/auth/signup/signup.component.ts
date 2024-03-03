09bd60945e58a64e15404352adb8b27e7f9bbec1
Mark Tiu <mark.tiu@prudentialguarantee.com>
2024-03-03 11:56:24 +0800

    Add validators for username

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -9,7 +9,12 @@ import {FormControl, FormGroup} from "@angular/forms";
 export class SignupComponent implements OnInit{
 
   authForm = new FormGroup({
-    username: new FormControl(),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/^[a-z0-9]+$/)
+    ]),
     password: new FormControl(),
     passwordConfirmation: new FormControl()
   })
