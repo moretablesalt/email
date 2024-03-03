7e6f2a4339b61505b6aec9affe70acc558a83904
Mark Tiu <mark.tiu@prudentialguarantee.com>
2024-03-03 11:13:04 +0800

    Add Sign Up component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
   declarations: [
-    SigninComponent
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     CommonModule,
