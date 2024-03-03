32d440e8c269ef61171e48d56b30ba06f1ac07cb
Mark Tiu <mark.tiu@prudentialguarantee.com>
2024-03-03 11:22:54 +0800

    Lazy load sign in (example only)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'sign-in',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  }
+];
