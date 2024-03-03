750a809c1789f89f926ff8e16246e6f648ef5dc9
Mark Tiu <mark.tiu@prudentialguarantee.com>
2024-03-04 07:08:54 +0800

    Finalize async validator (unique username)

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,7 +1,7 @@
 import {inject, Injectable} from "@angular/core";
 import {AbstractControl, AsyncValidator, ValidationErrors, Validator} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
-import {Observable, of} from "rxjs";
+import {catchError, map, Observable, of} from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class UniqueUsername implements AsyncValidator{
@@ -12,9 +12,26 @@ export class UniqueUsername implements AsyncValidator{
 
     const SERVER = 'https://api.angular-email.com/auth/username'
 
-    console.log(this.http);
-    // return this.http.post<any>(SERVER, {username: value});
-    return of(null);
+
+    return this.http.post<any>(SERVER, {username: value}).pipe(
+      map(value => {
+        if (value.available) {
+          return null;
+        }
+
+        return null;
+      }),
+      catchError(err => {
+        console.log(err);
+        if (err.status = 422) {
+          return of({nonUniqueUsername: true});
+        } else {
+          return of({noConnection: true});
+
+        }
+      })
+    );
+    // return of(null);
   }
 
 }
