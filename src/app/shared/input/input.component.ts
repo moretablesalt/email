75a493d83b2390afd9ec492d4da2ac55d2f215c7
Mark Tiu <mark.tiu@prudentialguarantee.com>
2024-03-04 12:04:49 +0800

    Refactor input to be resuable

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {
+  AbstractControl, Form, FormControl,
+  ReactiveFormsModule,
+} from "@angular/forms";
+
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+  ],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
-export class InputComponent {
+export class InputComponent implements OnInit{
+  @Input() control!: AbstractControl;
+  controlAsFormControl!: FormControl;
+
+  ngOnInit(): void {
+    // need FormControl
+    this.controlAsFormControl = this.control as FormControl<any>;
+  }
+
 
 }
