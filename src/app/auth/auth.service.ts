0728b068ce8280c5776c0d8bb45d9e0f7cfcfcf7
Mark Tiu <mark.tiu@prudentialguarantee.com>
2024-03-05 18:56:01 +0800

    Behavior subject to keep track if logged in

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject, tap} from "rxjs";
 
 interface UsernameAvailableResponse {
   available: boolean;
@@ -27,11 +28,16 @@ export class AuthService {
   USERNAME_PATH = '/auth/username';
   SIGNUP_PATH = '/auth/signup';
 
+  signedin$ = new BehaviorSubject(false);
+
   userNameAvailable(username: string) {
     return this.http.post<UsernameAvailableResponse>(this.SERVER_URL + this.USERNAME_PATH, {username: username});
   }
 
   signUp(credentials: SignupCredentials) {
-    return this.http.post<SignupResponse>(this.SERVER_URL + this.SIGNUP_PATH, credentials);
+    return this.http.post<SignupResponse>(this.SERVER_URL + this.SIGNUP_PATH, credentials).pipe(
+      // error will skip tap
+      tap(() => {this.signedin$.next(true)})
+    );
   }
 }
