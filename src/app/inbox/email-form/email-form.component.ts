e7c27d564032e9f7674d8c35c61ba4342c45fe2d
Mark Tiu <mark.tiu@prudentialguarantee.com>
2024-03-06 18:05:03 +0800

    Add validators and disable by default

--- src/app/inbox/email-form/email-form.component.ts
+++ src/app/inbox/email-form/email-form.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Email} from "../email";
-import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {InputComponent} from "../../shared/input/input.component";
 
 @Component({
@@ -23,10 +23,10 @@ export class EmailFormComponent implements OnInit{
   ngOnInit(): void {
     const {subject, to, from, text} = this.email
     this.emailForm = new FormGroup({
-      to: new FormControl(to),
-      from: new FormControl(from),
-      subject: new FormControl(subject),
-      text: new FormControl(text)
+      to: new FormControl(to, [Validators.required, Validators.email]),
+      from: new FormControl({value: from, disabled: true}),
+      subject: new FormControl(subject, [Validators.required]),
+      text: new FormControl(text, [Validators.required])
     });
   }
 
