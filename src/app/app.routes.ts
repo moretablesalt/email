4155b89f2bf960cd399aa0417cf6fd4a1d923bc8
Mark Tiu <mark.tiu@prudentialguarantee.com>
2024-03-06 11:10:15 +0800

    Create guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import {AuthGuard} from "./auth/auth.guard";
 
 export const routes: Routes = [
   {
@@ -7,6 +8,7 @@ export const routes: Routes = [
   },
   {
     path: 'inbox',
+    canLoad: [AuthGuard],
     loadChildren: () => import('./inbox/inbox.module').then(m => m.InboxModule)
   }
 ];
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,11 @@
+import {CanLoad, Route, UrlSegment, UrlTree} from "@angular/router";
+import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanLoad {
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+
+}
