6d93948c5d029676dca47de615bb2cb6c8cc3c96
Mark Tiu <mark.tiu@prudentialguarantee.com>
2024-03-06 08:09:57 +0800

    Automate signout when navigating to signout compoennt

--- src/app/auth/signout/signout.component.ts
+++ src/app/auth/signout/signout.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signout',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
   templateUrl: './signout.component.html',
   styleUrl: './signout.component.css'
 })
-export class SignoutComponent {
+export class SignoutComponent implements OnInit{
+  constructor(private authService: AuthService, private router: Router) {
+  }
+  ngOnInit() {
+    this.authService.signout().subscribe(() => {
+      this.router.navigateByUrl('/');
+    } );
+  }
 
 }
