29ec5b0a971a4a5e1ab5582b2585197006a57056
Mark Tiu <mark.tiu@prudentialguarantee.com>
2024-03-06 14:21:54 +0800

    params vs snapshot (ActivatedRote)

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -13,9 +13,15 @@ export class EmailShowComponent implements OnInit{
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    // 2 ways to accesss
+    // route params
     this.route.params.subscribe(({id}) => {
       console.log('route: ' + id);
     });
+
+    // snapshot
+    // might cause an issue if component is resused
+    console.log(this.route.snapshot.params['id']);
   }
 
 
