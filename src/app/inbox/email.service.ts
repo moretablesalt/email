04ebdc79da66cb7ec8c75bebe1587d131056211d
Mark Tiu <mark.tiu@prudentialguarantee.com>
2024-03-06 12:58:24 +0800

    Create getEmail in service

--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+interface EmailSummary {
+  id: string;
+  subject: string;
+  from: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor() { }
+  private SERVER_URL = 'https://api.angular-email.com'
+
+  USERNAME_PATH = '/auth/username';
+  SIGNUP_PATH = '/auth/signup';
+
+  constructor(private http: HttpClient) { }
+
+  getEmails() {
+    return this.http.get<EmailSummary[]>(`${this.SERVER_URL}/emails`);
+  }
+
 }
