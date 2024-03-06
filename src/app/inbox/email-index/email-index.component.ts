7c1e6323e808087e979227ef5b0188573d13abf9
Mark Tiu <mark.tiu@prudentialguarantee.com>
2024-03-06 13:17:08 +0800

    Implement inbox index

--- src/app/inbox/email-index/email-index.component.ts
+++ src/app/inbox/email-index/email-index.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EmailService, EmailSummary} from "../email.service";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-email-index',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './email-index.component.html',
   styleUrl: './email-index.component.css'
 })
-export class EmailIndexComponent {
+export class EmailIndexComponent implements OnInit {
 
+  emails: EmailSummary[] = [];
+  constructor(private emailService: EmailService) { }
+
+  ngOnInit(): void {
+    this.emailService.getEmails().subscribe(emails => {
+      this.emails = emails;
+      console.log(emails);
+    });
+  }
 }
--- src/app/inbox/home/home.component.ts
+++ src/app/inbox/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {EmailIndexComponent} from "../email-index/email-index.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    EmailIndexComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
